45f713e8e3349d417c7769a15753875d4f10c702
SP19 <SP19@SP22.local>
2018-04-19 22:31:56 -0300

    Added match sreen

---
+++ Obligatorio_1/EventTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  EventTableViewCell.swift
+//  Obligatorio_1
+//
+//  Created by SP19 on 19/4/18.
+//  Copyright © 2018 UCU. All rights reserved.
+//
+
+import UIKit
+
+class EventTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var eventTime:       UILabel!
+    @IBOutlet weak var leftEmoji:       UILabel!
+    @IBOutlet weak var rightPlayerName: UILabel!
+    @IBOutlet weak var leftPlayerName:  UILabel!
+    @IBOutlet weak var rightEmoji:      UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Obligatorio_1/MatchViewController.swift
+++ Obligatorio_1/MatchViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MatchViewController.swift
 //  Obligatorio_1
 //
 //  Created by SP19 on 19/4/18.
@@ -8,37 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate, 
-UITableViewDataSource {
+class MatchViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var eventsTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.delegate   = self
-        tableView.dataSource = self
+        eventsTableView.delegate   = self
+        eventsTableView.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 5
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "id", for: indexPath) as! MatchTableViewCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "eventCellId", for: indexPath)
         return cell
     }
+    
 
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 200
-    }
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        performSegue(withIdentifier: "showDetails", sender: nil)
-    }
+
 }
-
