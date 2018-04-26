e843c066ef097cc78e987fa5448c14dbfe23dd17
Nicolás Mazza <mazzaniclas@gmail.com>
2018-04-26 18:43:19 -0300

    dummy data

--- Obligatorio_1/ViewController.swift
+++ Obligatorio_1/ViewController.swift
@@ -19,7 +19,36 @@ UITableViewDataSource {
         tableView.delegate   = self
         tableView.dataSource = self
         
-        TeamMember(name:"Roberto Carlos", temporalId: "69", countryTeam: "Rusia", club: "Un Club")
+        let stadium: Stadium = Stadium(name: "Luzhniki Stadium", logo: /*LOGO*/)
+        let date:    Date    = Date(timeIntervalSinceReferenceDate: 118800)
+        
+        let event1:  Event   = Event('00',⚽,'Le gol con lo pie', "Russia") 
+        let event2:  Event   = Event('20',⚽,'Le gol con lo pie', "Russia") 
+        let event3:  Event   = Event('30',⚽,'Le gol con lo pie', "Uruguay") 
+        let event4:  Event   = Event('60',⚽,'Le gol con lo pie', "Russia") 
+        let eventList: Array<Event> = [event1, event2, event3, event4]
+
+        let russiaTeamMemberNames: Array<String> = ["Igor Akinfeev","Aleksandr Selikhov","Andrey Lunyov","Roman Neustadter","Ilya Kutepov","Andrei Semyonov"]
+        let russiaTeamMemberIds:   Array<String> = ["1","12","99","3","4","5"]
+        let russiaTeamMemberClubs: Array<String> = ["Russia CSKA Moscow","Russia Spartak Moscow","Russia Zenit Saint Petersburge","Turkey Fenerbahce","Russia Spartak Moscow","Russia Akhmat Grozny"]
+        russiaTeamMembers = createTeamMembers(name: russiaTeamMemberNames, temporalId: russiaTeamMemberIds, countryTeam: "Russia", club: russiaTeamMemberClubs)
+        russia = countryTeam(name: "Russia", russiaTeamMember,logo: /*"LOGO"*/)
+
+        let uruguayTeamMemberNames: Array<String> = ["Fernando Muslera","Martín Silva","Martín Campaña","Maxi Pereira","Diego Godín","José María Giménez"]
+        let uruguayTeamMemberIds:   Array<String> = ["1","23","12","16","3","2"]
+        let uruguayTeamMemberClubs: Array<String> = ["Turkey Galatasaray","Brazil Vasco da Gama","Argentina Independiente","Portugal Porto","Spain Atlético Madrid","Spain Atlético Madrid"]
+        createTeamMembers(name: uruguayTeamMemberNames, temporalId: uruguayTeamMemberIds, countryTeam: "Uruguay", club: uruguayTeamMemberClubs)
+        uruguay = countryTeam(name: "Uruguay", uruguayTeamMember,logo: /*"LOGO"*/)
+
+        Match(homeTeam: russia, awayTeam: uruguay,3,1,date)
+    }
+
+    func createTeamMembers(teamMemberNames: Array<String>, teamMemberIds: Array<String>, countryTeam: String, teamMemberClubs: Array<String>) -> Array<TeamMember>{
+        var teamMembers = Array<TeamMember>
+        for index in 1...6 {
+            teamMembers.append(TeamMember(name:teamMemberNames[index], temporalId:teamMemberIds[index], countryTeam:countryTeam, club:teamMemberClubs))
+        }
+        return teamMembers
     }
 
     override func didReceiveMemoryWarning() {
